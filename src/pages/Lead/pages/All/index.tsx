e615f7473e9968f92feb0c0dd2eb7db3eab3182c
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-09 05:41:55 +0600

    updated; button

--- src/pages/Lead/pages/All/index.tsx
+++ src/pages/Lead/pages/All/index.tsx
@@ -1,5 +1,5 @@
 import { useGetLeads } from "@/queries/leads";
-import { Button, Container, Grid, IconButton, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import React from "react";
 import LeadColumn from "./components/LeadColumn";
 import DataTable from "@components/Datatable";
@@ -7,47 +7,50 @@ import { usePaginate } from "@tam11a/react-use-hooks";
 import { Icon } from "@iconify/react";
 
 const Leads: React.FC = () => {
-  const { page, setPage, getQueryParams, limit, setLimit } = usePaginate();
+	const { page, setPage, getQueryParams, limit, setLimit } = usePaginate();
 
-  const { data, isLoading } = useGetLeads(getQueryParams());
-  console.log(data?.data?.data);
+	const { data, isLoading } = useGetLeads(getQueryParams());
 
-  return (
-    <>
-      <div>
-        <div className="flex flex-row items-center justify-between">
-          <Typography variant="h5" fontWeight={700}>
-            All Leads
-          </Typography>
-          <div>
-            <Button variant={"contained"}>
-              Create New{" "}
-              <IconButton sx={{ fontSize: "large" }} color="primary">
-                <Icon className="text-white" icon="mdi:plus-box-outline" />
-              </IconButton>
-            </Button>
-          </div>
-        </div>
-      </div>
-      <div className="w-full">
-        <DataTable
-          columns={LeadColumn()}
-          // columns={[]}
-          rows={data?.data?.data || []}
-          // rows={[]}
-          isLoading={isLoading}
-          // getRowId={(r: any) => r?._id}
-          // ss pagination
-          rowCount={data?.data?.total || 0}
-          paginationMode={"server"}
-          page={page}
-          onPageChange={setPage}
-          pageSize={limit}
-          onPageSizeChange={setLimit}
-        />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<div className="flex flex-row items-center justify-between">
+					<Typography
+						variant="h5"
+						fontWeight={700}
+					>
+						All Leads
+					</Typography>
+					<div>
+						<Button
+							size="small"
+							variant={"contained"}
+							endIcon={<Icon icon="mdi:plus-box-outline" />}
+						>
+							Create New{" "}
+						</Button>
+					</div>
+				</div>
+			</div>
+			<div className="w-full">
+				<DataTable
+					columns={LeadColumn()}
+					// columns={[]}
+					rows={data?.data?.data || []}
+					// rows={[]}
+					isLoading={isLoading}
+					// getRowId={(r: any) => r?._id}
+					// ss pagination
+					rowCount={data?.data?.total || 0}
+					paginationMode={"server"}
+					page={page}
+					onPageChange={setPage}
+					pageSize={limit}
+					onPageSizeChange={setLimit}
+				/>
+			</div>
+		</>
+	);
 };
 
 export default Leads;
