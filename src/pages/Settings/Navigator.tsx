b35bc17795f51fff9a4186e3e522081e3249a3fd
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-09 04:11:00 +0600

    updated: settings

--- src/pages/Settings/Navigator.tsx
+++ src/pages/Settings/Navigator.tsx
@@ -8,42 +8,57 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { SETTINGS_ROUTES } from "./routes/paths";
 
 const items: MenuProps["items"] = [
-  {
-    label: "Personal",
-    key: SETTINGS_ROUTES.PERSONAL,
-    icon: <Icon icon="ic:twotone-person-pin" className="text-xl" />,
-  },
-  {
-    label: "Security",
-    key: SETTINGS_ROUTES.SECURITY,
-    icon: <Icon icon="ic:twotone-lock-person" className="text-xl" />,
-  },
-  {
-    label: "Session",
-    key: SETTINGS_ROUTES.SESSION,
-    icon: <Icon icon="ic:twotone-lock-person" className="text-xl" />,
-  },
+	{
+		label: "Personal",
+		key: SETTINGS_ROUTES.PERSONAL,
+		icon: (
+			<Icon
+				icon="ic:twotone-person-pin"
+				className="text-xl"
+			/>
+		),
+	},
+	{
+		label: "Security",
+		key: SETTINGS_ROUTES.SECURITY,
+		icon: (
+			<Icon
+				icon="ic:twotone-lock-person"
+				className="text-xl"
+			/>
+		),
+	},
+	{
+		label: "Session",
+		key: SETTINGS_ROUTES.SESSION,
+		icon: (
+			<Icon
+				icon="ic:twotone-lock-person"
+				className="text-xl"
+			/>
+		),
+	},
 ];
 
 const Navigator: React.FC = () => {
-  // To get the current location pathname
-  let location = useLocation();
+	// To get the current location pathname
+	let location = useLocation();
 
-  // To route
-  const navigate = useNavigate();
-  const onClick: MenuProps["onClick"] = (e) => {
-    navigate(e.key);
-  };
+	// To route
+	const navigate = useNavigate();
+	const onClick: MenuProps["onClick"] = (e) => {
+		navigate(e.key);
+	};
 
-  return (
-    <Menu
-      onClick={onClick}
-      selectedKeys={[location.pathname?.split?.("/")[3] || ""]}
-      mode="horizontal"
-      items={items}
-      className={"mb-1"}
-    />
-  );
+	return (
+		<Menu
+			onClick={onClick}
+			selectedKeys={[location.pathname?.split?.("/")[3] || ""]}
+			mode="horizontal"
+			items={items}
+			className={"mb-5 justify-center"}
+		/>
+	);
 };
 
 export default Navigator;
