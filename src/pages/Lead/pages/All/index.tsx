bc8bffbfe7968d7306e8f60d787b35d6428ff291
sanchitadevi <thesanchitadevi@gmail.com>
2023-09-09 04:43:10 +0600

    data fetched on table

--- src/pages/Lead/pages/All/index.tsx
+++ src/pages/Lead/pages/All/index.tsx
@@ -1,32 +1,42 @@
 import { useGetLeads } from "@/queries/leads";
-import { Container, Grid, Typography } from "@mui/material";
+import { Button, Container, Grid, IconButton, Typography } from "@mui/material";
 import React from "react";
 import LeadColumn from "./components/LeadColumn";
 import DataTable from "@components/Datatable";
 import { usePaginate } from "@tam11a/react-use-hooks";
+import { Icon } from "@iconify/react";
 
 const Leads: React.FC = () => {
   const { page, setPage, getQueryParams, limit, setLimit } = usePaginate();
 
   const { data, isLoading } = useGetLeads(getQueryParams());
+  console.log(data?.data?.data);
 
   return (
     <>
       <div>
-        <div>
-          <Typography variant="subtitle1" fontWeight={700}>
+        <div className="flex flex-row items-center justify-between">
+          <Typography variant="h5" fontWeight={700}>
             All Leads
           </Typography>
+          <div>
+            <Button variant={"contained"}>
+              Create New{" "}
+              <IconButton sx={{ fontSize: "large" }} color="primary">
+                <Icon className="text-white" icon="mdi:plus-box-outline" />
+              </IconButton>
+            </Button>
+          </div>
         </div>
       </div>
       <div className="w-full">
         <DataTable
           columns={LeadColumn()}
           // columns={[]}
-          // rows={data?.data?.data || []}
-          rows={[]}
+          rows={data?.data?.data || []}
+          // rows={[]}
           isLoading={isLoading}
-          getRowId={(r: any) => r?._id}
+          // getRowId={(r: any) => r?._id}
           // ss pagination
           rowCount={data?.data?.total || 0}
           paginationMode={"server"}
