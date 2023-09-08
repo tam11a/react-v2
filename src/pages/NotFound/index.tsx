1ccb0c5cbc2864fdb2aafd26afa710ed72612851
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-09 04:14:34 +0600

    added: 404

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,7 +1,24 @@
+import { Avatar } from "@mui/material";
 import React from "react";
 
 const NotFound: React.FC = () => {
-	return <div>NotFound</div>;
+	return (
+		<>
+			<Avatar
+				src={"/404.svg"}
+				sx={{
+					position: "absolute",
+					top: "50%",
+					left: "50%",
+					transform: "translate(-50%, -50%)",
+					width: "90vw",
+					maxWidth: "460px",
+					height: "auto",
+				}}
+				variant={"square"}
+			/>
+		</>
+	);
 };
 
 export default NotFound;
