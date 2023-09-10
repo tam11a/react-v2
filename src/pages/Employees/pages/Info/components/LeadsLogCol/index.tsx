cfb9db4fea49acc7612dcab642977d55595c3d98
Asatter833 <asatter833@gmail.com>
2023-09-11 03:41:58 +0600

    employee info complete

--- src/pages/Employees/pages/Info/components/LeadsLogCol/index.tsx
+++ src/pages/Employees/pages/Info/components/LeadsLogCol/index.tsx
@@ -1,7 +1,77 @@
-import React from "react";
+import { GridColumns } from "@mui/x-data-grid";
+import { IDataTable } from "@components/Datatable/types";
+import { useNavigate } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import { Icon } from "@iconify/react";
 
-const LeadsLogCol: React.FC = () => {
-  return <div>index</div>;
+const LeadsLogCol = (): GridColumns<IDataTable> => {
+  const navigate = useNavigate();
+  return [
+    {
+      headerName: "Lead Title",
+      headerAlign: "center",
+      field: "name",
+      align: "center",
+      // width: 200,
+      flex: 1,
+      sortable: false,
+      // hide: true,
+    },
+    {
+      headerName: "Assign Person",
+      headerAlign: "center",
+      field: "assigned_to",
+      minWidth: 100,
+      flex: 1,
+      align: "center",
+    },
+    {
+      headerName: "Status",
+      headerAlign: "center",
+      field: "status",
+      minWidth: 100,
+      flex: 1,
+      align: "center",
+    },
+    {
+      headerName: "Followup Date",
+      headerAlign: "center",
+      field: "followup_date",
+      minWidth: 100,
+      flex: 1,
+      align: "center",
+    },
+    {
+      headerName: "Source",
+      headerAlign: "center",
+      field: "media",
+      minWidth: 100,
+      flex: 1,
+      align: "center",
+    },
+    {
+      headerName: "Action",
+      field: "action",
+      width: 100,
+      minWidth: 80,
+      // flex: 1,
+      flex: 1,
+      headerAlign: "center",
+      align: "center",
+      renderCell: (data: any) => (
+        <>
+          <IconButton
+            sx={{ fontSize: "large" }}
+            color="primary"
+            onClick={() => navigate(`/app/leads/details/${data.row?.id}`)}
+            // disabled={!checkAccess(defaultPermissions.EMPLOYEES.FULL)}
+          >
+            <Icon icon="icon-park-solid:info" />
+          </IconButton>
+        </>
+      ),
+    },
+  ];
 };
 
 export default LeadsLogCol;
--- src/pages/Employees/pages/Info/pages/Overview/index.tsx
+++ src/pages/Employees/pages/Info/pages/Overview/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import PersonalInfo from "./personalInfo";
 import Label from "@components/Label";
 import DataTable from "@components/Datatable";
-// import LeadsLogCol from "../../components/LeadsLogCol";
+import LeadsLogCol from "../../components/LeadsLogCol";
 
 const Overview: React.FC = () => {
   const { page, setPage, limit, setLimit } = usePaginate();
@@ -12,17 +12,20 @@ const Overview: React.FC = () => {
       {/* details */}
       <PersonalInfo />
       <>
-        <div className="mx-auto m-4">
-          <Label className="text-md font-medium">Interested Properties</Label>
+        <div className="m-4">
+          <Label className="text-base font-semibold my-3 text-text-light">
+            Leads Log
+          </Label>
           <DataTable
-            // columns={LeadsLogCol()}
-            columns={[]}
+            columns={LeadsLogCol()}
+            // columns={[]}
             // rows={data?.data?.data || []}
             rows={[]}
             isLoading={false}
             // getRowId={(r: any) => r?._id}
             // ss pagination
             // rowCount={data?.data?.total || 0}
+            checkboxSelection
             paginationMode={"server"}
             page={page}
             onPageChange={setPage}
