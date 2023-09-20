1edc38273846a471a83fd2cd54c359089a0027c1
Asatter833 <asatter833@gmail.com>
2023-09-20 23:42:37 +0600

    routed dashboard and navigator removed

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import DashboardRoutes from "./routes";
-import Navigator from "./Navigator";
 
 const Dashboard: React.FC = () => {
   return (
     <>
-      <Navigator />
       <DashboardRoutes />
     </>
   );
--- src/routes/privateRoutes.ts
+++ src/routes/privateRoutes.ts
@@ -2,36 +2,36 @@ import { lazy } from "react";
 import { PRIVATE_ROUTES } from "./paths";
 
 export const privateRoutes = [
-	{
-		path: PRIVATE_ROUTES.DASHBOARD,
-		Component: lazy(() => import("@pages/Dummy")),
-	},
-	{
-		path: PRIVATE_ROUTES.NOTFOUND,
-		Component: lazy(() => import("@pages/NotFound")),
-	},
-	{
-		path: PRIVATE_ROUTES.LEADS,
-		Component: lazy(() => import("@pages/Lead")),
-	},
-	{
-		path: PRIVATE_ROUTES.EMPLOYEES,
-		Component: lazy(() => import("@pages/Employees")),
-	},
-	{
-		path: PRIVATE_ROUTES.PROPERTIES,
-		Component: lazy(() => import("@pages/Properties")),
-	},
-	{
-		path: PRIVATE_ROUTES.MEDIA,
-		Component: lazy(() => import("@pages/Media")),
-	},
-	{
-		path: PRIVATE_ROUTES.ROLES,
-		Component: lazy(() => import("@pages/Roles")),
-	},
-	{
-		path: PRIVATE_ROUTES.SETTINGS,
-		Component: lazy(() => import("@pages/Settings")),
-	},
+  {
+    path: PRIVATE_ROUTES.DASHBOARD,
+    Component: lazy(() => import("@pages/Dashboard")),
+  },
+  {
+    path: PRIVATE_ROUTES.NOTFOUND,
+    Component: lazy(() => import("@pages/NotFound")),
+  },
+  {
+    path: PRIVATE_ROUTES.LEADS,
+    Component: lazy(() => import("@pages/Lead")),
+  },
+  {
+    path: PRIVATE_ROUTES.EMPLOYEES,
+    Component: lazy(() => import("@pages/Employees")),
+  },
+  {
+    path: PRIVATE_ROUTES.PROPERTIES,
+    Component: lazy(() => import("@pages/Properties")),
+  },
+  {
+    path: PRIVATE_ROUTES.MEDIA,
+    Component: lazy(() => import("@pages/Media")),
+  },
+  {
+    path: PRIVATE_ROUTES.ROLES,
+    Component: lazy(() => import("@pages/Roles")),
+  },
+  {
+    path: PRIVATE_ROUTES.SETTINGS,
+    Component: lazy(() => import("@pages/Settings")),
+  },
 ];
