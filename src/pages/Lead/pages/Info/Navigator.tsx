79de9df4dbd9918fa71971f0b60cf33e63a21880
Asatter833 <asatter833@gmail.com>
2023-09-21 17:13:46 +0600

    followup date added

--- src/pages/Lead/pages/Info/Navigator.tsx
+++ src/pages/Lead/pages/Info/Navigator.tsx
@@ -3,9 +3,9 @@ import type { MenuProps } from "antd";
 import { Button, Menu, Select, Alert } from "antd";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import {
-	useDeleteLead,
-	useGetLeadsById,
-	useUpdateLeadsById,
+  useDeleteLead,
+  useGetLeadsById,
+  useUpdateLeadsById,
 } from "@/queries/leads";
 import moment from "moment";
 import { message } from "@components/antd/message";
@@ -13,103 +13,106 @@ import handleResponse from "@/utilities/handleResponse";
 import useLeadStatus from "@/hooks/useLeadStatus";
 import { useToggle } from "@tam11a/react-use-hooks";
 import TransferDialog from "./components/TransferDialog";
+import FollowupDialog from "./components/FollowupDialog";
 
 const Navigator: React.FC = () => {
-	// To get the current location pathname
-	let location = useLocation();
+  // To get the current location pathname
+  let location = useLocation();
 
-	// To route
-	const navigate = useNavigate();
+  // To route
+  const navigate = useNavigate();
 
-	const { id } = useParams();
-	const { data } = useGetLeadsById(id);
-	const leadInfo = data?.data?.data;
+  const { id } = useParams();
+  const { data } = useGetLeadsById(id);
+  const leadInfo = data?.data?.data;
 
-	const { mutateAsync: updateLead } = useUpdateLeadsById();
+  const { mutateAsync: updateLead } = useUpdateLeadsById();
 
-	const { leadStatus } = useLeadStatus();
+  const { leadStatus } = useLeadStatus();
 
-	const { mutateAsync: deleteLead } = useDeleteLead();
+  const { mutateAsync: deleteLead } = useDeleteLead();
 
-	const onRestore = async (id: any) => {
-		message.open({
-			type: "loading",
-			content: "Restoring Lead..",
-			duration: 0,
-		});
+  const onRestore = async (id: any) => {
+    message.open({
+      type: "loading",
+      content: "Restoring Lead..",
+      duration: 0,
+    });
 
-		const res = await handleResponse(() =>
-			deleteLead({
-				id,
-				params: {
-					restore: true,
-				},
-			})
-		);
-		message.destroy();
-		if (res.status) {
-			message.success("Lead restored successfully!");
-			return true;
-		} else {
-			message.error(res.message);
-			return false;
-		}
-	};
+    const res = await handleResponse(() =>
+      deleteLead({
+        id,
+        params: {
+          restore: true,
+        },
+      })
+    );
+    message.destroy();
+    if (res.status) {
+      message.success("Lead restored successfully!");
+      return true;
+    } else {
+      message.error(res.message);
+      return false;
+    }
+  };
 
-	const items: MenuProps["items"] = [
-		{
-			label: "Update",
-			key: `/app/leads/details/${id}/update`,
-		},
-		{
-			label: "View",
-			key: `/app/leads/details/${id}`,
-		},
-	];
+  const items: MenuProps["items"] = [
+    {
+      label: "Update",
+      key: `/app/leads/details/${id}/update`,
+    },
+    {
+      label: "View",
+      key: `/app/leads/details/${id}`,
+    },
+  ];
 
-	const onClick: MenuProps["onClick"] = (e) => {
-		navigate(e.key);
-	};
+  const onClick: MenuProps["onClick"] = (e) => {
+    navigate(e.key);
+  };
 
-	const onValid = async (d: any) => {
-		message.open({
-			type: "loading",
-			content: `Updating information...`,
-			duration: 0,
-		});
-		const res = await handleResponse(
-			() =>
-				updateLead({
-					id,
-					data: d,
-				}),
-			[200]
-		);
-		message.destroy();
-		if (res.status) message.success("Information updated successfully!");
-		else message.error(res.message);
-	};
+  const onValid = async (d: any) => {
+    message.open({
+      type: "loading",
+      content: `Updating information...`,
+      duration: 0,
+    });
+    const res = await handleResponse(
+      () =>
+        updateLead({
+          id,
+          data: d,
+        }),
+      [200]
+    );
+    message.destroy();
+    if (res.status) message.success("Information updated successfully!");
+    else message.error(res.message);
+  };
 
-	const { state: openTransfer, toggleState: onCloseTransfer } =
-		useToggle(false);
+  const { state: openTransfer, toggleState: onCloseTransfer } =
+    useToggle(false);
+  const { state: openFollowup, toggleState: onCloseFollowup } =
+    useToggle(false);
 
-	return (
-		<>
-			<div className="flex flex-row items-center justify-between gap-2 p-3 text-text border-b">
-				<h1 className="text-2xl md:text-3xl font-bold">Lead Details</h1>
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between gap-2 p-3 text-text border-b">
+        <h1 className="text-2xl md:text-3xl font-bold">Lead Details</h1>
 
-				<div className="flex flex-row gap-2 items-center justify-end">
-					<span className="hidden md:flex flex-row items-center gap-2">
-						<Select
-							dropdownMatchSelectWidth={false}
-							bordered={false}
-							size="large"
-							value={leadInfo?.status.label}
-							onChange={(value) => onValid({ status_id: value })}
-							options={leadStatus}
-							className="bg-[#E7F5FC]  text-text-light"
-						/>
-						{/* <Select
+        <div className="flex flex-row gap-2 items-center justify-end">
+          <span className="hidden md:flex flex-row items-center gap-2">
+            <Select
+              dropdownMatchSelectWidth={false}
+              bordered={false}
+              size="large"
+              value={leadInfo?.status.label}
+              onChange={(value) => onValid({ status_id: value })}
+              options={leadStatus}
+              className="bg-[#E7F5FC]  text-text-light"
+            />
+            {/* <Select
 							dropdownMatchSelectWidth={false}
 							bordered={false}
 							size="large"
@@ -124,49 +127,60 @@ const Navigator: React.FC = () => {
 							]}
 							className="bg-[#E7F5FC]  text-text-light"
 						/> */}
-						<Button
-							type="primary"
-							size="large"
-							className="bg-[#E7F5FC] text-text max-w-fit"
-							block
-							onClick={() => onCloseTransfer()}
-						>
-							Transfer
-						</Button>
-						<TransferDialog
-							open={openTransfer}
-							onClose={onCloseTransfer}
-							lead_id={id}
-							assigned_to={leadInfo?.assigned_to}
-						/>
-					</span>
-					<Menu
-						onClick={onClick}
-						selectedKeys={[location.pathname?.split?.("/")[3] || ""]}
-						mode="horizontal"
-						items={items}
-						className={"border-b-0 min-w-[130px] max-w-[40px]"}
-					/>
-				</div>
-			</div>
-			{leadInfo?.deleted_at && (
-				<Alert
-					message={`This lead was deleted at 
+            <Button
+              type="primary"
+              size="large"
+              className="bg-[#E7F5FC] text-text max-w-fit"
+              block
+              onClick={() => onCloseTransfer()}
+            >
+              Transfer
+            </Button>
+            <TransferDialog
+              open={openTransfer}
+              onClose={onCloseTransfer}
+              lead_id={id}
+              assigned_to={leadInfo?.assigned_to}
+            />
+            <Button
+              type="primary"
+              size="large"
+              className="bg-[#E7F5FC] text-text max-w-fit"
+              block
+              onClick={() => onCloseFollowup()}
+            >
+              Followup
+            </Button>
+            <FollowupDialog
+              open={openFollowup}
+              onClose={onCloseFollowup}
+              lead_id={id}
+              followup_date={leadInfo?.followup_date}
+            />
+          </span>
+          <Menu
+            onClick={onClick}
+            selectedKeys={[location.pathname?.split?.("/")[3] || ""]}
+            mode="horizontal"
+            items={items}
+            className={"border-b-0 min-w-[100px] max-w-[40px]"}
+          />
+        </div>
+      </div>
+      {leadInfo?.deleted_at && (
+        <Alert
+          message={`This lead was deleted at 
           ${moment(leadInfo?.deleted_at).calendar()}.`}
-					banner
-					action={
-						<Button
-							size="small"
-							type="text"
-							onClick={() => onRestore(id)}
-						>
-							RESTORE
-						</Button>
-					}
-				/>
-			)}
-		</>
-	);
+          banner
+          action={
+            <Button size="small" type="text" onClick={() => onRestore(id)}>
+              RESTORE
+            </Button>
+          }
+        />
+      )}
+    </>
+  );
 };
 
 export default Navigator;
--- src/queries/leads/index.ts
+++ src/queries/leads/index.ts
@@ -3,116 +3,144 @@ import { ICreateLead, IUpdateLead } from "./type";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 const getLeads = (params: any) => {
-	return instance.get(`/leads`, {
-		params,
-	});
+  return instance.get(`/leads`, {
+    params,
+  });
 };
 
 export const useGetLeads = (params: any) => {
-	return useQuery(["get-all-leads", params], () => getLeads(params));
+  return useQuery(["get-all-leads", params], () => getLeads(params));
 };
 
 const createLead = (data: ICreateLead | any) => {
-	return instance.post("/leads", data);
+  return instance.post("/leads", data);
 };
 
 export const useCreateLead = () => {
-	const queryClient = useQueryClient();
-	return useMutation(createLead, {
-		onSuccess: () => queryClient.invalidateQueries(["get-all-leads"]),
-	});
+  const queryClient = useQueryClient();
+  return useMutation(createLead, {
+    onSuccess: () => queryClient.invalidateQueries(["get-all-leads"]),
+  });
 };
 
 const getLeadStatus = (params: any) => {
-	return instance.get(`/lead-status`, {
-		params,
-	});
+  return instance.get(`/lead-status`, {
+    params,
+  });
 };
 
 export const useGetLeadStatus = (params: any) => {
-	return useQuery(["get-all-lead-status", params], () => getLeadStatus(params));
+  return useQuery(["get-all-lead-status", params], () => getLeadStatus(params));
 };
 
 const getLeadsById = (id?: string) => {
-	return instance.get(`/leads/${id}`);
+  return instance.get(`/leads/${id}`);
 };
 
 export const useGetLeadsById = (id?: string) => {
-	return useQuery(["get-all-leads-by-id", id], () => getLeadsById(id), {
-		enabled: !!id,
-	});
+  return useQuery(["get-all-leads-by-id", id], () => getLeadsById(id), {
+    enabled: !!id,
+  });
 };
 
 const updateLeadsById = ({
-	id,
-	data,
+  id,
+  data,
 }: {
-	id?: string | number;
-	data: IUpdateLead | any;
+  id?: string | number;
+  data: IUpdateLead | any;
 }) => {
-	return instance.patch(`/leads/${id}`, {
-		...data,
-	});
+  return instance.patch(`/leads/${id}`, {
+    ...data,
+  });
 };
 
 export const useUpdateLeadsById = () => {
-	const query = useQueryClient();
-	return useMutation(updateLeadsById, {
-		onSuccess: () => {
-			query.invalidateQueries(["get-all-leads"]);
-			query.invalidateQueries(["get-all-leads-by-id"]);
-		},
-	});
+  const query = useQueryClient();
+  return useMutation(updateLeadsById, {
+    onSuccess: () => {
+      query.invalidateQueries(["get-all-leads"]);
+      query.invalidateQueries(["get-all-leads-by-id"]);
+    },
+  });
+};
+
+const followupLead = ({
+  id,
+  date,
+}: {
+  id?: string | number;
+  date: string | any;
+}) => {
+  return instance.put(
+    `/leads/${id}`,
+    {},
+    {
+      params: {
+        date,
+      },
+    }
+  );
+};
+
+export const useFollowupLead = () => {
+  const query = useQueryClient();
+  return useMutation(followupLead, {
+    onSuccess: () => {
+      query.invalidateQueries(["get-all-leads"]);
+      query.invalidateQueries(["get-all-leads-by-id"]);
+    },
+  });
 };
 
 const deleteLead = ({ id, params }: { id: number | string; params?: any }) => {
-	return instance.delete(`/leads/${id}`, {
-		params,
-	});
+  return instance.delete(`/leads/${id}`, {
+    params,
+  });
 };
 
 export const useDeleteLead = () => {
-	const query = useQueryClient();
-	return useMutation(deleteLead, {
-		onSuccess: () => {
-			query.invalidateQueries(["get-all-leads"]);
-			query.invalidateQueries(["get-leads-by-id"]);
-		},
-	});
+  const query = useQueryClient();
+  return useMutation(deleteLead, {
+    onSuccess: () => {
+      query.invalidateQueries(["get-all-leads"]);
+      query.invalidateQueries(["get-leads-by-id"]);
+    },
+  });
 };
 
 const interestedProperties = (id?: string) => {
-	return instance.get(`/leads/${id}/interested-properties`);
+  return instance.get(`/leads/${id}/interested-properties`);
 };
 
 export const useGetInterestedProperties = (id?: string) => {
-	return useQuery(["get-lead-interested-properties", id], () =>
-		interestedProperties(id)
-	);
+  return useQuery(["get-lead-interested-properties", id], () =>
+    interestedProperties(id)
+  );
 };
 
 const postInterest = ({
-	lead_id,
-	property_id,
+  lead_id,
+  property_id,
 }: {
-	lead_id: number | string;
-	property_id: number | string;
+  lead_id: number | string;
+  property_id: number | string;
 }) => {
-	return instance.post(
-		`/leads/${lead_id}/interested-properties`,
-		{},
-		{
-			params: {
-				property_id,
-			},
-		}
-	);
+  return instance.post(
+    `/leads/${lead_id}/interested-properties`,
+    {},
+    {
+      params: {
+        property_id,
+      },
+    }
+  );
 };
 
 export const usePostInterest = () => {
-	const queryClient = useQueryClient();
-	return useMutation(postInterest, {
-		onSuccess: () =>
-			queryClient.invalidateQueries(["get-lead-interested-properties"]),
-	});
+  const queryClient = useQueryClient();
+  return useMutation(postInterest, {
+    onSuccess: () =>
+      queryClient.invalidateQueries(["get-lead-interested-properties"]),
+  });
 };
