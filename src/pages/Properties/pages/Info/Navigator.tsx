f47ab35d244b91d5200abd576d36e4640e1f218b
tam11a <ibrahimsadiktamim@gmail.com>
2023-09-21 23:52:23 +0600

    updated

--- src/pages/Properties/pages/Info/Navigator.tsx
+++ src/pages/Properties/pages/Info/Navigator.tsx
@@ -10,107 +10,124 @@ import { message } from "@components/antd/message";
 import moment from "moment";
 
 const Navigator: React.FC = () => {
-  const { id } = useParams();
+	const { id } = useParams();
 
-  //trash alart
-  const { data } = useGetPropertiesById(id);
-  const propertyInfo = data?.data?.data;
-  console.log(propertyInfo);
+	//trash alart
+	const { data } = useGetPropertiesById(id);
+	const propertyInfo = data?.data?.data;
 
-  const { mutateAsync: deleteProperty } = useDeleteProperty();
+	const { mutateAsync: deleteProperty } = useDeleteProperty();
 
-  const onRestore = async (id: any) => {
-    message.open({
-      type: "loading",
-      content: "Restoring Property..",
-      duration: 0,
-    });
+	const onRestore = async (id: any) => {
+		message.open({
+			type: "loading",
+			content: "Restoring Property..",
+			duration: 0,
+		});
 
-    const res = await handleResponse(() =>
-      deleteProperty({
-        id,
-        params: {
-          restore: true,
-        },
-      })
-    );
-    message.destroy();
-    if (res.status) {
-      message.success("Property restored successfully!");
-      return true;
-    } else {
-      message.error(res.message);
-      return false;
-    }
-  };
+		const res = await handleResponse(() =>
+			deleteProperty({
+				id,
+				params: {
+					restore: true,
+				},
+			})
+		);
+		message.destroy();
+		if (res.status) {
+			message.success("Property restored successfully!");
+			return true;
+		} else {
+			message.error(res.message);
+			return false;
+		}
+	};
 
-  const items1: MenuProps["items"] = [
-    {
-      label: "Overview",
-      key: ROUTES.OVERVIEW,
-      icon: <Icon icon="ph:book-open-duotone" className="text-xl" />,
-    },
-    {
-      label: "Update",
-      key: `/app/properties/details/${id}/update-flat`,
-    },
-    {
-      label: "View All Properties",
-      key: `/app/properties`,
-    },
-  ];
-  const items2: MenuProps["items"] = [
-    {
-      label: "Overview",
-      key: ROUTES.OVERVIEW,
-      icon: <Icon icon="ph:book-open-duotone" className="text-xl" />,
-    },
-    {
-      label: "Update",
-      key: `/app/properties/details/${id}/update-land`,
-    },
-    {
-      label: "View All Properties",
-      key: `/app/properties`,
-    },
-  ];
-  // To get the current location pathname
+	const items1: MenuProps["items"] = [
+		{
+			label: "Overview",
+			key: ROUTES.OVERVIEW,
+			icon: (
+				<Icon
+					icon="ph:book-open-duotone"
+					className="text-xl"
+				/>
+			),
+		},
+		{
+			label: "Update",
+			key: `/app/properties/details/${id}/update-flat`,
+			disabled: !!propertyInfo?.deleted_at,
+		},
+		{
+			label: "View All Properties",
+			key: `/app/properties`,
+		},
+	];
+	const items2: MenuProps["items"] = [
+		{
+			label: "Overview",
+			key: ROUTES.OVERVIEW,
+			icon: (
+				<Icon
+					icon="ph:book-open-duotone"
+					className="text-xl"
+				/>
+			),
+		},
+		{
+			label: "Update",
+			key: `/app/properties/details/${id}/update-land`,
+		},
+		{
+			label: "View All Properties",
+			key: `/app/properties`,
+		},
+	];
+	// To get the current location pathname
 
-  let location = useLocation();
+	let location = useLocation();
 
-  // To route
-  const navigate = useNavigate();
-  const onClick: MenuProps["onClick"] = (e) => {
-    navigate(e.key);
-  };
+	// To route
+	const navigate = useNavigate();
+	const onClick: MenuProps["onClick"] = (e) => {
+		navigate(e.key);
+	};
 
-  return (
-    <>
-      <div className="flex md:flex-row flex-col md:items-center justify-between gap-2 px-2 text-text border-b">
-        <p className="text-md font-bold">properties / {id}</p>
-
-        <Menu
-          onClick={onClick}
-          selectedKeys={[location.pathname?.split?.("/")[3] || ""]}
-          mode="horizontal"
-          items={propertyInfo?.type === "FLAT" ? items1 : items2}
-          className={"border-b-0 w-full max-w-[450px]"}
-        />
-      </div>
-      {propertyInfo?.deleted_at && (
-        <Alert
-          message={`This property was deleted at 
+	return (
+		<>
+			<div className="flex md:flex-row flex-col md:items-center justify-between gap-2 px-2 text-text border-b">
+				<p className="text-md font-bold">properties / {id}</p>
+				<Menu
+					onClick={onClick}
+					selectedKeys={[
+						location.pathname?.split?.("/")[5] || "",
+						location.pathname,
+					]}
+					mode="horizontal"
+					items={propertyInfo?.type === "FLAT" ? items1 : items2}
+					className={"border-b-0 w-full max-w-[450px]"}
+				/>
+			</div>
+			{propertyInfo?.deleted_at && (
+				<Alert
+					message={`This property was deleted at 
           ${moment(propertyInfo?.deleted_at).calendar()}.`}
-          banner
-          action={
-            <Button size="small" type="text" onClick={() => onRestore(id)}>
-              UNDO
-            </Button>
-          }
-        />
-      )}
-    </>
-  );
+					banner
+					action={
+						<Button
+							color="black"
+							size="small"
+							type="dashed"
+							onClick={() => onRestore(id)}
+						>
+							RESTORE
+						</Button>
+					}
+				/>
+			)}
+		</>
+	);
 };
 
 export default Navigator;
--- src/queries/properties/index.ts
+++ src/queries/properties/index.ts
@@ -3,74 +3,74 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { ICreateProperty, IUpdateProperty } from "./types";
 
 const getProperties = (params: any) => {
-  return instance.get(`/assets`, {
-    params,
-  });
+	return instance.get(`/assets`, {
+		params,
+	});
 };
 
 export const useGetProperties = (params: any) => {
-  return useQuery(["get-all-properties", params], () => getProperties(params));
+	return useQuery(["get-all-properties", params], () => getProperties(params));
 };
 
 const getPropertiesById = (id?: string) => {
-  return instance.get(`/assets/${id}`);
+	return instance.get(`/assets/${id}`);
 };
 
 export const useGetPropertiesById = (id?: string) => {
-  return useQuery(
-    ["get-all-properties-by-id", id],
-    () => getPropertiesById(id),
-    {
-      enabled: !!id,
-    }
-  );
+	return useQuery(
+		["get-all-properties-by-id", id],
+		() => getPropertiesById(id),
+		{
+			enabled: !!id,
+		}
+	);
 };
 
 const createProperty = (data: ICreateProperty | any) => {
-  return instance.post("/assets", data);
+	return instance.post("/assets", data);
 };
 
 export const useCreateProperty = () => {
-  const queryClient = useQueryClient();
-  return useMutation(createProperty, {
-    onSuccess: () => queryClient.invalidateQueries(["get-all-properties"]),
-  });
+	const queryClient = useQueryClient();
+	return useMutation(createProperty, {
+		onSuccess: () => queryClient.invalidateQueries(["get-all-properties"]),
+	});
 };
 
 const updatePropertyById = ({
-  id,
-  data,
+	id,
+	data,
 }: {
-  id?: string;
-  data: IUpdateProperty | any;
+	id?: string;
+	data: IUpdateProperty | any;
 }) => {
-  return instance.patch(`/assets/${id}`, {
-    ...data,
-  });
+	return instance.patch(`/assets/${id}`, {
+		...data,
+	});
 };
 
 export const useUpdatePropertyById = () => {
-  const query = useQueryClient();
-  return useMutation(updatePropertyById, {
-    onSuccess: () => {
-      query.invalidateQueries(["get-all-properties"]);
-      query.invalidateQueries(["get-properties-by-id"]);
-    },
-  });
+	const query = useQueryClient();
+	return useMutation(updatePropertyById, {
+		onSuccess: () => {
+			query.invalidateQueries(["get-all-properties"]);
+			query.invalidateQueries(["get-properties-by-id"]);
+		},
+	});
 };
 
 const deleteProperty = ({ id, params }: { id: number; params?: any }) => {
-  return instance.delete(`/assets/${id}`, {
-    params,
-  });
+	return instance.delete(`/assets/${id}`, {
+		params,
+	});
 };
 
 export const useDeleteProperty = () => {
-  const query = useQueryClient();
-  return useMutation(deleteProperty, {
-    onSuccess: () => {
-      query.invalidateQueries(["get-all-properties"]);
-      query.invalidateQueries(["get-property-by-id"]);
-    },
-  });
+	const query = useQueryClient();
+	return useMutation(deleteProperty, {
+		onSuccess: () => {
+			query.invalidateQueries(["get-all-properties"]);
+			query.invalidateQueries(["get-properties-by-id"]);
+		},
+	});
 };
